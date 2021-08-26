27434dc35e571cbad5c76802cf3f3c78e112b08f
kalebmcc <kalebl33mccullough@gmail.com>
2021-08-26 12:08:28 -0500

    Added Javascript Framework

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-//Select
-const start = document.querySelector('.start')
-const score = document.querySelector('.score')
+// ---------------SELECTIONS---------------
+const start = document.querySelector('.start');
+const score = document.querySelector('.score');
 
-const colorContainer = document.querySelector('.color-container')
+const colorContainer = document.querySelector('.color-container');
 const blue = document.querySelector('#blue');
 const red = document.querySelector('#red');
 const yellow = document.querySelector('#yellow');
 const green = document.querySelector('#green');
+
+// ---------------GLOBAL_VARIABLES---------------
+let playerScore = 0
+let colorSequence = []
+let playerSequence = []
+
+
+// ---------------FUNCTIONS---------------
+
+function startGame() {
+    //starts game
+}
+
+function playGame() {
+    //clicking sequence
+}
+
+function colorSequence() {
+    //color sequence
+}
+
+function updateScore() {
+    //updates score variable
+}
+
+function wrong() {
+    //wrong answer
+    //modal with final score
+}
+
+
+
+
+
+// ---------------EVENTS---------------
+
+start.addEventListener('click', startGame())
+
+colorContainer.addEventListener('click', playGame())
