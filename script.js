03cf57d5e2bb77920d4fa0978d2e8866254c62fc
kalebmcc <kalebl33mccullough@gmail.com>
2021-08-26 11:53:26 -0500

    Basic Visual Setup

---
+++ script.js
@@ -0,0 +1,9 @@
+//Select
+const start = document.querySelector('.start')
+const score = document.querySelector('.score')
+
+const colorContainer = document.querySelector('.color-container')
+const blue = document.querySelector('#blue');
+const red = document.querySelector('#red');
+const yellow = document.querySelector('#yellow');
+const green = document.querySelector('#green');
